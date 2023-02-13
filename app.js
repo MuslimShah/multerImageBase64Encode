aeb59f92f0cc29e654943de37631747e595abf93
Muslim shah <smuslimshah7@gmail.com>
2023-02-13 16:45:53 +0500

    updated with image storing

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ var crypto = require("crypto");
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.set('view engine', 'ejs');
+
+//for fast uploading optional in api
 app.get('/upload', (req, res) => {
-    res.render('index');
-})
+        res.render('index');
+    })
+    //------------------------------
+
 app.get('/image/:hash', (req, res) => {
     //getting hash from user url
     const hash = req.params.hash;
     const filePath = path.join(__dirname, 'uploads', `${hash}.png`);
     //reading data from file of specified hash name
-    res.sendFile(filePath)
-        // fs.readFile(filePath, (err, content) => {
-        //     if (!err) {
-        //         res.json(content)
-        //     } else {
-        //         res.json({ error: 'image not found' })
-        //     }
-        // })
+    res.sendFile(filePath);
 })
 
 app.post('/upload', multer({}).single("image"), function(req, res, next) {
@@ -34,14 +30,12 @@ app.post('/upload', multer({}).single("image"), function(req, res, next) {
     //storing hash values
     const hashPath = path.join(__dirname, 'hashFiles', `${encodedSha256}`);
     fs.writeFile(hashPath, encodedSha256, (err) => {
-        if (!err) {
-            console.log(`hash stored ${encodedSha256}`);
-        }
-    })
-
-    //saving file with name of hash calculated
+            if (!err) {
+                console.log(`hash stored ${encodedSha256}`);
+            }
+        })
+        //saving file with name of hash calculated
     const imagePath = path.join(__dirname, 'uploads', `${encodedSha256}.png`);
-
     fs.writeFile(imagePath, req.file.buffer, (err) => {
             if (!err) {
                 console.log('image saved on server');
