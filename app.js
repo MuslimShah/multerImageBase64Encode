9c45eed0d53295eb7b514a0cff929d22042963f4
Muslim shah <smuslimshah7@gmail.com>
2023-02-13 16:36:30 +0500

    updated according to requirement

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var crypto = require("crypto");
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const upload = multer({});
+
 app.set('view engine', 'ejs');
 app.get('/upload', (req, res) => {
     res.render('index');
@@ -13,32 +13,44 @@ app.get('/upload', (req, res) => {
 app.get('/image/:hash', (req, res) => {
     //getting hash from user url
     const hash = req.params.hash;
-    const filePath = path.join(__dirname, 'hashFiles', `${hash}`);
+    const filePath = path.join(__dirname, 'uploads', `${hash}.png`);
     //reading data from file of specified hash name
-    fs.readFile(filePath, (err, content) => {
-        if (!err) {
-            res.json({ img: `${content}.png` })
-        } else {
-            res.json({ error: 'image not found' })
-        }
-    })
+    res.sendFile(filePath)
+        // fs.readFile(filePath, (err, content) => {
+        //     if (!err) {
+        //         res.json(content)
+        //     } else {
+        //         res.json({ error: 'image not found' })
+        //     }
+        // })
 })
 
-app.post('/upload', upload.single('image'), (req, res) => {
-    //getting image from buffer
+app.post('/upload', multer({}).single("image"), function(req, res, next) {
+    //encoding image to base64 
     const encoded = req.file.buffer.toString('base64');
-    //converting to sha256
-    const encodedSha256 = crypto.createHash('sha256').update(encoded).digest('hex')
-        //path to store hased value
-    const filePath = path.join(__dirname, 'hashFiles', `${encodedSha256}`);
-    //storing hash in a file with the same name as hash and with no extension
-    fs.writeFile(filePath, encodedSha256, (err) => {
+    //converting base64 to hash256
+    const encodedSha256 = crypto.createHash('sha256').update(encoded).digest('hex');
+
+    //storing hash values
+    const hashPath = path.join(__dirname, 'hashFiles', `${encodedSha256}`);
+    fs.writeFile(hashPath, encodedSha256, (err) => {
+        if (!err) {
+            console.log(`hash stored ${encodedSha256}`);
+        }
+    })
+
+    //saving file with name of hash calculated
+    const imagePath = path.join(__dirname, 'uploads', `${encodedSha256}.png`);
+
+    fs.writeFile(imagePath, req.file.buffer, (err) => {
             if (!err) {
-                console.log('image converted and stored as hash');
+                console.log('image saved on server');
             }
         })
-        //return base64 to user
-    res.json({ encoding: "base64", encodedImage: encoded });
+        //sending success message and hash for finding image    
+    res.json({ msg: 'file uploaded successfully', hash: encodedSha256 });
 })
+
+
 
 app.listen(PORT, () => console.log(`connected port:${PORT}`));
